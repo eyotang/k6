9ebb8d2a793e1d74675a74dbbd0b9ba41920e5a1
tangyq <tangyq@yoozoo.com>
2018-09-19 21:22:03 +0800

    add close

--- js/modules/k6/tcp/tcp.go
+++ js/modules/k6/tcp/tcp.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"net"
 	"strconv"
-	
+
 	"github.com/pkg/errors"
 )
 
 type TCP struct {
-	ctx           context.Context
-	conn          *net.Conn
+	ctx  context.Context
+	conn net.Conn
 }
 
 func New() *TCP {
@@ -21,12 +21,18 @@ func (t *TCP) Connect(ctx context.Context, host string, port uint64) (err error)
 	var (
 		conn net.Conn
 	)
-	
+
 	if conn, err = net.Dial("tcp", host+":"+strconv.FormatUint(port, 10)); err != nil {
 		errors.Wrapf(err, "net.Dial failed")
-		return 
+		return
 	}
-	
-	t.conn = &conn
-	return 
+
+	t.ctx = ctx
+	t.conn = conn
+	return
+}
+
+func (t *TCP) Close() (err error) {
+	err = t.conn.Close()
+	return
 }
