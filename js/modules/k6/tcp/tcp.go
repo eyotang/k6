084aadc4c3ad914aa84c1bbf9dd99b40d7855725
tangyq <tangyq@yoozoo.com>
2018-09-22 09:02:01 +0800

    use rps

--- js/modules/k6/tcp/tcp.go
+++ js/modules/k6/tcp/tcp.go
@@ -3,6 +3,7 @@ package tcp
 import (
 	"context"
 	"fmt"
+	"github.com/eyotang/k6/js/common"
 	"net"
 	"strconv"
 
@@ -23,6 +24,8 @@ func (t *TCP) Connect(ctx context.Context, host string, port uint64) (err error)
 		conn net.Conn
 	)
 
+	fmt.Println("Connect ------->")
+
 	if conn, err = net.Dial("tcp", host+":"+strconv.FormatUint(port, 10)); err != nil {
 		errors.Wrapf(err, "net.Dial failed")
 		return
@@ -34,6 +37,16 @@ func (t *TCP) Connect(ctx context.Context, host string, port uint64) (err error)
 }
 
 func (t *TCP) Send(ctx context.Context, data []byte) (err error) {
+
+	state := common.GetState(ctx)
+
+	// Check rate limit *after* we've prepared a request; no need to wait with that part.
+	if rpsLimit := state.RPSLimit; rpsLimit != nil {
+		if err = rpsLimit.Wait(ctx); err != nil {
+			return
+		}
+	}
+
 	fmt.Printf("Send  ====> message: %v\n", data)
 	return
 }
--- samples/tcp.js
+++ samples/tcp.js
@@ -1,20 +1,25 @@
 import tcp from 'k6/tcp';
 
+export let options = {
+	rps: 1,
+	vus: 1,
+	duration: '3s'
+};
+
 export function setup() {
+	tcp.connect("10.18.98.194", 22);
 	return {
 		format: ['<', 'i', 'h', 'i'] // '<ihi'
 	};
 }
 
 export default function(data) {
-	const response = tcp.connect("10.18.98.194", 22);
-
-	let msg = [147, 8, 1, 20, 25]
-	let headers = [msg.length, 1, 3]
-	let request = tcp.pack(data.format, headers, msg)
+	let msg = [147, 8, 1, 20, 25];
+	let headers = [msg.length, 1, 3];
+	let request = tcp.pack(data.format, headers, msg);
 	tcp.send(request);
-};
+}
 
 export function teardown(data) {
 	tcp.close();
-};
+}
